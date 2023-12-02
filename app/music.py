3f472eea8883f0e3944d5973b0cd3f6502579198
Semyon Bochkaryov <squizduos@gmail.com>
2023-12-02 12:12:23 +0300

    Frist version

--- app/music.py
+++ app/music.py
@@ -59,6 +59,7 @@ class FilterPlaylistButton(discord.ui.Button['Music']):
     def __init__(self, title: str, filter: str, player_view):
         super().__init__(style=discord.ButtonStyle.danger, label=title)
         self.filter = filter
+        self.page = 0
         self.player_view = player_view
 
     async def callback(self, interaction: discord.Interaction):
