3c0c3112b864928f0a6a1584c96da33e35fce0d6
Semyon Bochkaryov <squizduos@gmail.com>
2023-12-02 12:10:36 +0300

    Frist version

--- app/music.py
+++ app/music.py
@@ -29,27 +29,28 @@ class PlayButton(discord.ui.Button['Music']):
         print("Now playing: {}".format(target_file_path))
 
         def repeat(file_path):
-            if self.is_playing:
+            if self.bot.is_playing:
                 repeat_source = discord.PCMVolumeTransformer(
-                    discord.FFmpegPCMAudio(file_path, options="-b:a 128k", executable=self.bot.cfg.ffmpeg_executable)
+                    discord.FFmpegPCMAudio(file_path, options="-b:a 128k -stream_loop -1", executable=self.bot.cfg.ffmpeg_executable)
                 )
                 repeat_source.volume = self.bot.volume
 
                 if self.bot.ctx.voice_client.is_playing():
                     self.bot.ctx.voice_client.pause()
 
-                self.bot.ctx.voice_client.play(repeat_source, after=lambda e: repeat(file_path))
-                self.bot.ctx.voice_client.is_playing()
+                self.bot.ctx.voice_client.play(
+                    repeat_source, # after=lambda e: repeat(file_path)
+                )
+                # self.bot.ctx.voice_client.is_playing()
 
         if self.bot.ctx.voice_client.is_playing():
             self.bot.ctx.voice_client.pause()
 
-        self.is_playing = True
+        self.bot.is_playing = True
+
         repeat(target_file_path)
 
         self.bot.current = self.filename
-        # self.ctx.voice_client.play(source, after=lambda e: repeat(source))
-        # self.ctx.voice_client.is_playing()
 
         await interaction.response.edit_message(content=self.bot.caption())
 
@@ -64,36 +65,6 @@ class FilterPlaylistButton(discord.ui.Button['Music']):
         self.player_view.filter = self.filter
         self.player_view.refresh()
         await self.player_view.show(interaction)
-
-
-
-# class MusicList(discord.ui.View):
-#
-#     def __init__(self, *, timeout=180):
-#         super().__init__(timeout=timeout)
-#         files_list = os.listdir("files/")
-#         for f in files_list:
-#             button = discord.ui.Button(style=discord.ButtonStyle.secondary, label=f)
-#
-#     @discord.ui.button(label="Запустить музыку (только админ)", style=discord.ButtonStyle.green)
-#     async def gray_button(self, interaction:discord.Interaction, button: discord.ui.Button):
-#         if interaction.user.guild_permissions.administrator:
-#             await interaction.response.send_message(content=f"This is an edited button response!")
-#         else:
-#             await interaction.response.send_message(content=f"У вас нет прав для администрирования музыки!")
-
-
-# class LaunchListButton(discord.ui.View):
-#
-#     def __init__(self, *, timeout=180):
-#         super().__init__(timeout=timeout)
-#
-#     @discord.ui.button(label="Запустить музыку (только админ)", style=discord.ButtonStyle.green)
-#     async def gray_button(self, interaction:discord.Interaction, button: discord.ui.Button):
-#         if interaction.user.guild_permissions.administrator:
-#             await interaction.response.send_message(content=f"This is an edited button response!")
-#         else:
-#             await interaction.response.send_message(content=f"У вас нет прав для администрирования музыки!")
 
 
 class PlayerView(discord.ui.View):
@@ -129,12 +100,7 @@ class PlayerView(discord.ui.View):
         files_list = [element for element in self.files_list if self.filter.lower() in element.lower() or not self.filter]
         for file_name in files_list[self.page * self.__page_size__():(self.page + 1) * self.__page_size__()]:
             title = "".join(file_name.split(".")[:-1])
-            # print(file_name)
-            # play_button = discord.ui.Button(style=discord.ButtonStyle.secondary, label=file_name,
-            #                                 custom_id=f"play_{file_name}")
-            # play_button.file_name = file_name
-            # play_button.callback = lambda button_interaction: self.play_song(button_interaction, file_name)
-            # play_button.callback = None
+
             play_button = PlayButton(title=title, filename=file_name, bot=self.bot)
             buttons_list.append(play_button)
         return buttons_list
@@ -168,10 +134,12 @@ class PlayerView(discord.ui.View):
     async def stop_music(self, interaction: discord.Interaction, button: discord.ui.Button):
         if self.bot.ctx.voice_client.is_playing():
             self.bot.ctx.voice_client.pause()
-            self.bot.is_playing = False
+
+        self.bot.is_playing = False
         self.bot.current = "#NA"
 
         self.refresh()
+
         await self.show(interaction)
 
     @discord.ui.button(label="🔊", style=discord.ButtonStyle.green, row=0)
@@ -205,21 +173,13 @@ class LaunchButtonView(discord.ui.View):
             )
             await interaction.response.send_message(content=self.bot.caption(), view=player_view, ephemeral=True)
         else:
-            await interaction.response.edit_message(content=f"У вас нет прав для администрирования музыки!")
+            await interaction.response.send_message(content=f"У вас нет прав для администрирования музыки!")
 
 
 class MusicBot(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        # self.song_parent = db["files_dir"]
-        # print("Music files will be loaded from {}.".format(self.song_parent))
-        # self.song_database = db["songs"]
-        # print("songs: ", end="")
-        # for x in self.song_database:
-        #     print(x["song_keys"][0], ", ", end="")
-        # print("")
-        # self.playlist = []
         self.current = "#NA"
         self.ctx = None
         self.volume = 1
@@ -233,126 +193,6 @@ class MusicBot(commands.Cog):
 Громкость: `{self.volume:.0%}`
         """
 
-    async def change_volume(self, interaction: discord.Interaction):
-        if interaction.data["custom_id"] == "volume_down":
-            self.volume = max(self.volume - 0.2, 0)
-        else:
-            self.volume = min(self.volume + 0.2, 1)
-        if self.ctx.voice_client.source is not None:
-            self.ctx.voice_client.source.volume = self.volume
-
-    async def stop(self, interaction: discord.Interaction):
-        if self.ctx.voice_client.is_playing():
-            self.ctx.voice_client.stop()
-            self.is_playing = False
-        await interaction.response.edit_message(content=f"Сейчас ничего не воспроизводится.")
-
-    async def play_song(self, interaction: discord.Interaction):
-        print(interaction.data['custom_id'])
-        target_file_name = interaction.data['custom_id'][5:]
-        target_file_path = os.path.join(os.curdir, "../files", target_file_name)  # target_file_name
-
-        def repeat(file_path):
-            if self.is_playing:
-                repeat_source = discord.PCMVolumeTransformer(
-                    discord.FFmpegPCMAudio(file_path, options="-b:a 128k", executable=self.cfg.ffmpeg_executable)
-                )
-                repeat_source.volume = self.volume
-
-                if self.ctx.voice_client.is_playing():
-                    self.ctx.voice_client.stop()
-
-                self.ctx.voice_client.play(repeat_source, after=lambda e: repeat(file_path))
-                self.ctx.voice_client.is_playing()
-
-        if self.ctx.voice_client.is_playing():
-            self.ctx.voice_client.stop()
-
-        self.is_playing = True
-        repeat(target_file_path)
-        # self.ctx.voice_client.play(source, after=lambda e: repeat(source))
-        # self.ctx.voice_client.is_playing()
-
-        print("Now playing: {}".format(target_file_path))
-        await interaction.response.edit_message(content=f"Воспроизводится {target_file_path}")
-
-    async def show_files_list(self, interaction: discord.Interaction):
-        if not interaction:  # interaction.user.guild_permissions.administrator
-            await interaction.response.send_message(content=f"У вас нет прав для запуска музыки!", ephemeral=True)
-        else:
-            page = 0
-            edited = False
-            if "custom_id" in interaction.data and interaction.data["custom_id"].startswith("page_"):
-                page = int(interaction.data["custom_id"].replace("page_", ""))
-                edited = True
-
-            music_view = discord.ui.View()
-            files_list = os.listdir("../files/")
-
-            list_back_button = discord.ui.Button(
-                style=discord.ButtonStyle.primary,
-                label="◀️",
-                custom_id=f"page_{page - 1}",
-                row=0
-            )
-            if page == 0:
-                list_back_button.disabled = True
-            list_back_button.callback = self.show_files_list
-
-            volume_down_button = discord.ui.Button(
-                style=discord.ButtonStyle.primary,
-                label="⤵️",
-                custom_id=f"volume_down",
-                row=0
-            )
-            volume_down_button.callback = self.change_volume
-
-            stop_button = discord.ui.Button(
-                style=discord.ButtonStyle.primary,
-                label="⏹",
-                custom_id=f"stop",
-                row=0
-            )
-            stop_button.callback = self.stop
-
-            volume_up_button = discord.ui.Button(
-                style=discord.ButtonStyle.primary,
-                label="⤴️",
-                custom_id=f"volume_up",
-                row=0
-            )
-            volume_up_button.callback = self.change_volume
-
-            list_forward_button = discord.ui.Button(
-                style=discord.ButtonStyle.primary,
-                label="▶️",
-                custom_id=f"page_{page + 1}",
-                row=0
-            )
-            if len(files_list) < (page + 1) * 20:
-                list_forward_button.disabled = True
-            list_forward_button.callback = self.show_files_list
-
-            music_view.add_item(list_back_button)
-            music_view.add_item(volume_down_button)
-            music_view.add_item(stop_button)
-            music_view.add_item(volume_up_button)
-            music_view.add_item(list_forward_button)
-
-            for file_name in files_list[page * 20:(page + 1) * 20]:
-                play_button = discord.ui.Button(style=discord.ButtonStyle.secondary, label=file_name,
-                                                custom_id=f"play_{file_name}")
-                play_button.file_name = file_name
-                # play_button.callback = lambda button_interaction: self.play_song(button_interaction, file_name)
-                play_button.callback = self.play_song
-                music_view.add_item(play_button)
-
-            if edited:
-                await interaction.response.edit_message(view=music_view)
-            else:
-                await interaction.response.send_message(content=f"Сейчас ничего не воспроизводится.", view=music_view,
-                                                        ephemeral=True)
-
     @commands.command()
     async def join(self, ctx):
         channel = ctx.guild.voice_channels[0]
@@ -362,21 +202,11 @@ class MusicBot(commands.Cog):
         self.ctx = ctx
         await channel.connect()
 
-        # async def button_callback(interaction):
-        #     await interaction.response.edit_message(content='Button clicked!', view=None)
-        #
-        # button = Button(custom_id='button1', label='WOW button!', style=discord.ButtonStyle.green)
-        # button.callback = button_callback
-
-        # launchListView = discord.ui.View()
-        # launchListButton = discord.ui.Button(label="Запустить музыку (только админ)", style=discord.ButtonStyle.green)
-        # launchListButton.callback = lambda interaction: self.show_files_list(interaction)
-        # launchListView.add_item(launchListButton)
-
         launch_button_view = LaunchButtonView(bot=self)
         await channel.send(
             'Бот готов к запуску музыки!\nОбратите внимание, управлять музыкой может только администратор сервера.',
-            view=launch_button_view)
+            view=launch_button_view
+        )
 
     @commands.command()
     async def quit(self, ctx):
@@ -387,186 +217,6 @@ class MusicBot(commands.Cog):
         # ctx.voice_client.stop()
         await ctx.voice_client.disconnect()
 
-    # def parse(self, query):
-    #     play_args = self.parser.parse_args(query.split(" "))
-    #     target_songs = []
-    #     # process args!
-    #     if "artist" in play_args and play_args.artist is not None:
-    #         print("Query artists: ", end="")
-    #         print(play_args.artist)
-    #         for artist in play_args.artist:
-    #             artist_clean = artist.lower().replace(" ", "")  # sanitize
-    #             tmp = [x for x in self.song_database if artist_clean in x["artist_keys"]]
-    #             target_songs = [*target_songs, *tmp]
-    #     if "album" in play_args and play_args.album is not None:
-    #         print("Query album: ", end="")
-    #         print(play_args.album)
-    #         for album in play_args.album:
-    #             album_clean = album.lower().replace(" ", "")
-    #             tmp = [x for x in self.song_database if album_clean in x["album_keys"]]
-    #             target_songs = [*target_songs, *tmp]
-    #     if "song" in play_args and play_args.song is not None:
-    #         query_songs = [x.replace("\"", "").replace("\'", "") for x in  play_args.song]
-    #         print("Query songs: ", end="")
-    #         print(query_songs)
-    #         if len(target_songs) != 0: # filter!
-    #             target_songs_new = []
-    #             for song in query_songs:
-    #                 song_clean = song.lower().replace(" ", "")
-    #                 tmp = [x for x in target_songs if song_clean in x["song_keys"]]
-    #                 target_songs_new = [*target_songs_new, *tmp]
-    #             target_songs = target_songs_new
-    #         else:
-    #             for song in query_songs:
-    #                 song_clean = song.lower().replace(" ", "")
-    #                 tmp = [x for x in self.song_database if song_clean in x["song_keys"]]
-    #                 target_songs = [*target_songs, *tmp]
-
-    #     return target_songs
-
-    # @commands.command()
-    # async def add(self, ctx, *, query):
-    #     self.ctx = ctx
-
-    #     target_songs = self.parse(query)
-    #     self.playlist = [*self.playlist, *target_songs]
-
-    # @commands.command()
-    # async def play(self, ctx, *, query):
-    #     self.ctx = ctx
-    #     print("Incoming query: ", query)
-
-    #     target_songs = self.parse(query)
-
-    #     self.playlist = [*self.playlist, *target_songs]
-    #     print("Current playlist: ", end="")
-    #     for x in self.playlist:
-    #         print("    {} by {}".format(x["song_keys"][0], x["artist_keys"][0]))
-
-    #     if ctx.voice_client.is_playing():
-    #         ctx.voice_client.stop()
-    #     else:
-    #         await self.play_song(None)
-
-    # async def play_song(self, error):
-    #     if error is not None:
-    #         print("Error: {}".format(e))
-    #         return
-    #     print("Checking playlist...")
-    #     if len(self.playlist) == 0:
-    #         await self.ctx.send("Playlist empty")
-    #         self.is_playing = False
-    #         return
-
-    #     print("looking for the file")
-    #     target_file_path = os.path.join(self.song_parent, self.playlist[0]["filename"])
-    #     target_song = self.playlist[0]["song_keys"][0]
-    #     target_artist = self.playlist[0]["artist_keys"][0]
-    #     del self.playlist[0]
-
-    #     await self.ensure_voice(self.ctx)
-
-    #     source = discord.PCMVolumeTransformer(
-    #         discord.FFmpegPCMAudio(target_file_path, options="-b:a 128k")
-    #     )
-    #     source.volume = self.volume
-    #     await self.ctx.send("Now playing: {} by {}".format(target_song, target_artist))
-    #     self.current = "{} by {}".format(target_song, target_artist)
-    #     self.ctx.voice_client.play(
-    #         source,
-    #         after=self.dispatch_play_song
-    #     )
-    #     print("Now playing: {} by {}".format(target_song, target_artist))
-
-    # def dispatch_play_song(self, e):
-    #     print("dispatch!")
-    #     if e is not None:
-    #         print("Error: ", end="")
-    #         print(e)
-    #         return
-
-    #     # asyncio.run(self.play_song(None), self.bot.loop)
-    #     coro = self.play_song(None)
-    #     fut = asyncio.run_coroutine_threadsafe(coro, self.bot.loop)
-    #     try:
-    #         print("fut.result")
-    #         fut.result()
-    #     except:
-    #         pass
-
-    #     return
-
-    # @commands.command()
-    # async def pause(self, ctx):
-    #     if ctx.voice_client.is_playing():
-    #         ctx.voice_client.pause()
-
-    # @commands.command()
-    # async def resume(self, ctx):
-    #     if ctx.voice_client.is_paused():
-    #         ctx.voice_client.resume()
-
-    # @commands.command()
-    # async def skip(self, ctx, *, how_many=0):
-    #     m_del = int(how_many)
-    #     m_del = m_del if m_del <= len(self.playlist) else len(self.playlist)
-    #     for _ in range(m_del):
-    #         del self.playlist[0]
-    #     # await self.play_song(None)
-    #     ctx.voice_client.stop()
-
-    # @commands.command()
-    # async def show(self, ctx):
-    #     to_show = [
-    #         "Now playing: " + self.current,
-    #         "Songs in playlist: "
-    #     ]
-    #     if len(self.playlist) == 0:
-    #         to_show[-1] = "Playlist empty"
-    #     for i, song in enumerate(self.playlist):
-    #         to_add = "{0:02d}. ".format(i) + song["song_keys"][0] + " by " + song["artist_keys"][0]
-    #         to_show.append(to_add)
-
-    #     await ctx.send("\n".join(to_show))
-
-    # @commands.command()
-    # async def quit(self, ctx):
-    #     self.playlist = []
-
-    #     ctx.voice_client.stop()
-
-    #     await ctx.voice_client.disconnect()
-
-    # @commands.command()
-    # async def q(self, ctx):
-    #     await self.quit(ctx)
-
-    # @commands.command()
-    # async def volume(self, ctx, volume: int):
-    #     if ctx.voice_client is None:
-    #         return await ctx.send("Not connected to a voice channel.")
-
-    #     self.volume = volume / 100
-    #     if ctx.voice_client.source is not None:
-    #         ctx.voice_client.source.volume = self.volume
-    #     await ctx.send("Changed volume to {}%".format(volume))
-
-    # @play.before_invoke
-    # async def ensure_voice(self, ctx):
-    #     if ctx.voice_client is None:
-    #         if ctx.author.voice:
-    #             await ctx.author.voice.channel.connect()
-    #         else:
-    #             await ctx.send("You are not connected to a voice channel.")
-    #             raise commands.CommandError("Author not connected to a voice channel.")
-    #     elif ctx.voice_client.is_playing():
-    #         pass
-
-    # # @commands.command()
-    # @discord.app_commands.command(name="hello")
-    # async def hello(self, interaction: discord.Interaction, member: discord.Member):
-    #     """Says hello"""
-    #     await interaction.response.send_message(f'Hello!', ephemeral=True)
 
 
 def setup(bot):
